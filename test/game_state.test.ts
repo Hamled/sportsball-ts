0502400bc536b7d175c8736c66fa348134110f2c
Charles Ellis <hamled@hamled.name>
2019-10-25 11:37:12 -0700

    Refactor player scoring > 0 test cases
    The test suite for each of the scoring possibilities is largely the
    same, so we should DRY it up by moving the parts that are the same into
    a single definition of the test suite.
    
    This necessarily means the individual tests are a lot more terse, and
    require reading through more of the code to understand any individual
    test. However, I think it might be worth it to avoid having several
    hundred more lines of code in total to say the same thing.

--- test/game_state.test.ts
+++ test/game_state.test.ts
@@ -110,10 +110,8 @@ describe('gameState', () => {
     away: 9, home: 2, turn: {team: Team.Away, bases: []}
   }];
 
-  describe('called with player score of 4', () => {
-    const playerScore = 4;
-
-    const scoring4TestSuite = (bases: boolean[], addPoints: number): void => {
+  const scoringTestSuite = (playerScore: number) =>
+    (bases: boolean[], addPoints: number, {newBases, basesTestName}): void => {
       testCases.map(s => withBases(s, bases)).forEach(state => {
         describe(`when away is ${state.away} and home is ${state.home}`, () => {
           const prevTeam = state.turn.team;
@@ -141,21 +139,28 @@ describe('gameState', () => {
             expect(newState.turn.team).toEqual(state.turn.team);
           });
 
-          it('clears the bases', () => {
+          it(basesTestName, () => {
             const newState = gameState(state, playerScore);
 
-            expect(newState.turn.bases).toEqual([false, false, false]);
+            expect(newState.turn.bases).toEqual(newBases);
           });
         });
       });
     };
 
+  describe('called with player score of 4', () => {
+    const scoring4TestSuite = scoringTestSuite(4);
+    const newBasesDesc = {
+      newBases: [false, false, false],
+      basesTestName: 'clears all bases'
+    };
+
     describe('when bases are empty', () => {
-      scoring4TestSuite([false, false, false], 1); // Should add one point
+      scoring4TestSuite([false, false, false], 1, newBasesDesc);
     });
 
     describe('when bases are loaded', () => {
-      scoring4TestSuite([true, true, true], 4); // Should add four points
+      scoring4TestSuite([true, true, true], 4, newBasesDesc);
     });
 
     describe('when one player on base', () => {
@@ -166,7 +171,7 @@ describe('gameState', () => {
       ];
 
       basesStates.forEach(bases => {
-        scoring4TestSuite(bases, 2); // Should add two points
+        scoring4TestSuite(bases, 2, newBasesDesc);
       });
     });
 
@@ -178,57 +183,24 @@ describe('gameState', () => {
       ];
 
       basesStates.forEach(bases => {
-        scoring4TestSuite(bases, 3); // Should add three points
+        scoring4TestSuite(bases, 3, newBasesDesc);
       });
     });
   });
 
   describe('called with player score of 3', () => {
-    const playerScore = 3;
-
-    const scoring3TestSuite = (bases: boolean[], addPoints: number): void => {
-      testCases.map(s => withBases(s, bases)).forEach(state => {
-        describe(`when away is ${state.away} and home is ${state.home}`, () => {
-          const prevTeam = state.turn.team;
-
-          it(`adds ${addPoints} to current team (${teamName(state.turn.team)}) score`, () => {
-            const prevScore = teamScore(state, prevTeam);
-
-            const newState = gameState(state, playerScore);
-
-            expect(teamScore(newState, prevTeam)).toEqual(prevScore + addPoints);
-          });
-
-          it('does not change non-current team score', () => {
-            const otherTeam = prevTeam == Team.Home ? Team.Away : Team.Home;
-            const prevScore = teamScore(state, otherTeam);
-
-            const newState = gameState(state, playerScore);
-
-            expect(teamScore(newState, otherTeam)).toEqual(prevScore);
-          });
-
-          it('keeps same team current', () => {
-            const newState = gameState(state, playerScore);
-
-            expect(newState.turn.team).toEqual(state.turn.team);
-          });
-
-          it('has player on third base', () => {
-            const newState = gameState(state, playerScore);
-
-            expect(newState.turn.bases).toEqual([false, false, true]);
-          });
-        });
-      });
+    const scoring3TestSuite = scoringTestSuite(3);
+    const newBasesDesc = {
+      newBases: [false, false, true],
+      basesTestName: 'has player on third base'
     };
 
     describe('when bases are empty', () => {
-      scoring3TestSuite([false, false, false], 0); // Should add zero points
+      scoring3TestSuite([false, false, false], 0, newBasesDesc);
     });
 
     describe('when bases are loaded', () => {
-      scoring3TestSuite([true, true, true], 3); // Should add three points
+      scoring3TestSuite([true, true, true], 3, newBasesDesc);
     });
 
     describe('when one player on base', () => {
@@ -239,7 +211,7 @@ describe('gameState', () => {
       ];
 
       basesStates.forEach(bases => {
-        scoring3TestSuite(bases, 1); // Should add one point
+        scoring3TestSuite(bases, 1, newBasesDesc);
       });
     });
 
@@ -251,57 +223,26 @@ describe('gameState', () => {
       ];
 
       basesStates.forEach(bases => {
-        scoring3TestSuite(bases, 2); // Should add two points
+        scoring3TestSuite(bases, 2, newBasesDesc);
       });
     });
   });
 
   describe('called with player score of 2', () => {
-    const playerScore = 2;
-
-    const scoring2TestSuite = (bases: boolean[], addPoints: number, newBases: boolean[]): void => {
-      testCases.map(s => withBases(s, bases)).forEach(state => {
-        describe(`when away is ${state.away} and home is ${state.home}`, () => {
-          const prevTeam = state.turn.team;
-
-          it(`adds ${addPoints} to current team (${teamName(state.turn.team)}) score`, () => {
-            const prevScore = teamScore(state, prevTeam);
-
-            const newState = gameState(state, playerScore);
-
-            expect(teamScore(newState, prevTeam)).toEqual(prevScore + addPoints);
-          });
-
-          it('does not change non-current team score', () => {
-            const otherTeam = prevTeam == Team.Home ? Team.Away : Team.Home;
-            const prevScore = teamScore(state, otherTeam);
-
-            const newState = gameState(state, playerScore);
-
-            expect(teamScore(newState, otherTeam)).toEqual(prevScore);
-          });
-
-          it('keeps same team current', () => {
-            const newState = gameState(state, playerScore);
-
-            expect(newState.turn.team).toEqual(state.turn.team);
-          });
-
-          it('has player on second base, maybe on third', () => {
-            const newState = gameState(state, playerScore);
-
-            expect(newState.turn.bases).toEqual(newBases);
-          });
-        });
-      });
-    };
+    const scoring2TestSuite = scoringTestSuite(2);
 
     describe('when bases are empty', () => {
-      scoring2TestSuite([false, false, false], 0, [false, true, false]);
+      scoring2TestSuite([false, false, false], 0, {
+        newBases: [false, true, false],
+        basesTestName: 'has player on second'
+      });
     });
 
     describe('when bases are loaded', () => {
-      scoring2TestSuite([true, true, true], 2, [false, true, true]);
+      scoring2TestSuite([true, true, true], 2, {
+        newBases: [false, true, true],
+        basesTestName: 'has players on second and third'
+      });
     });
 
     describe('when one player on base', () => {
@@ -312,7 +253,10 @@ describe('gameState', () => {
       ];
 
       basesStates.forEach(bases => {
-        scoring2TestSuite(bases, 0, [false, true, true]);
+        scoring2TestSuite(bases, 0, {
+          newBases: [false, true, true],
+          basesTestName: 'has players on second and third'
+        });
       });
     });
 
@@ -324,69 +268,41 @@ describe('gameState', () => {
       ];
 
       basesStates.forEach(bases => {
-        scoring2TestSuite(bases, 1, [false, true, true]);
+        scoring2TestSuite(bases, 1, {
+          newBases: [false, true, true],
+          basesTestName: 'has players on second and third'
+        });
       });
     });
   });
 
   describe('called with player score of 1', () => {
-    const playerScore = 1;
-
-    const scoring1TestSuite = (bases: boolean[], addPoints: number, newBases: boolean[]): void => {
-      testCases.map(s => withBases(s, bases)).forEach(state => {
-        describe(`when away is ${state.away} and home is ${state.home}`, () => {
-          const prevTeam = state.turn.team;
-
-          it(`adds ${addPoints} to current team (${teamName(state.turn.team)}) score`, () => {
-            const prevScore = teamScore(state, prevTeam);
-
-            const newState = gameState(state, playerScore);
-
-            expect(teamScore(newState, prevTeam)).toEqual(prevScore + addPoints);
-          });
-
-          it('does not change non-current team score', () => {
-            const otherTeam = prevTeam == Team.Home ? Team.Away : Team.Home;
-            const prevScore = teamScore(state, otherTeam);
-
-            const newState = gameState(state, playerScore);
-
-            expect(teamScore(newState, otherTeam)).toEqual(prevScore);
-          });
-
-          it('keeps same team current', () => {
-            const newState = gameState(state, playerScore);
-
-            expect(newState.turn.team).toEqual(state.turn.team);
-          });
-
-          it('has player on first base, maybe on other bases', () => {
-            const newState = gameState(state, playerScore);
-
-            expect(newState.turn.bases).toEqual(newBases);
-          });
-        });
-      });
-    };
+    const scoring1TestSuite = scoringTestSuite(1);
 
     describe('when bases are empty', () => {
-      scoring1TestSuite([false, false, false], 0, [true, false, false]);
+      scoring1TestSuite([false, false, false], 0, {
+        newBases: [true, false, false],
+        basesTestName: 'has player on first'
+      });
     });
 
     describe('when bases are loaded', () => {
-      scoring1TestSuite([true, true, true], 1, [true, true, true]);
+      scoring1TestSuite([true, true, true], 1, {
+        newBases: [true, true, true],
+        basesTestName: 'has bases loaded'
+      });
     });
 
     describe('when one player on base', () => {
       const basesOutcomes = [
-        [[true, false, false], [true, true, false]],
-        [[false, true, false], [true, true, false]],
-        [[false, false, true], [true, false, true]]
+        [[true, false, false], [true, true, false], 'has players on first and second'],
+        [[false, true, false], [true, true, false], 'has players on first and second'],
+        [[false, false, true], [true, false, true], 'has players on first and third']
       ];
 
       basesOutcomes.forEach((testCase) => {
-        const [bases, outcomes] = testCase;
-        scoring1TestSuite(bases, 0, outcomes);
+        const [bases, newBases, basesTestName] = testCase;
+        scoring1TestSuite(bases as boolean[], 0, {newBases, basesTestName});
       });
     });
 
@@ -398,7 +314,10 @@ describe('gameState', () => {
       ];
 
       basesStates.forEach(bases => {
-        scoring1TestSuite(bases, 0, [true, true, true]);
+        scoring1TestSuite(bases, 0, {
+          newBases: [true, true, true],
+          basesTestName: 'has bases loaded'
+        });
       });
     });
   });
