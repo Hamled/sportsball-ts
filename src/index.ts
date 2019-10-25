068fb2000cb44b28f119da9a19a8bbc554937926
Charles Ellis <hamled@hamled.name>
2019-10-25 14:34:08 -0700

    Sportsball#getScore returns a string with score

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
+export type ScoredGameState = {away: number, home: number};
+type GameStateReducer = (state?: ScoredGameState, score?: number) => ScoredGameState;
+
 export class Sportsball {
+  constructor(private readonly reducer: GameStateReducer) {
+  }
+
   getScore(): string {
-    return '';
+    const state = this.reducer();
+    return `Home: ${state.home} Away: ${state.away}`;
   }
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,10 +1,23 @@
-import {Sportsball} from '../src';
+import {Sportsball, ScoredGameState} from '../src';
+
+const fakeReducerConstant = (_state?: ScoredGameState, _score?: number) => ({
+  away: 999,
+  home: 999
+});
 
 describe('Sportsball', () => {
   describe('#getScore', () => {
     it('is defined', () => {
-      const game = new Sportsball();
+      const game = new Sportsball(fakeReducerConstant);
 
       expect(game.getScore).toBeDefined();
     });
+
+    it('returns score string with home and away scores', () => {
+      // Check with a constant game state result
+      let game = new Sportsball(fakeReducerConstant);
+
+      expect(game.getScore()).toEqual('Home: 999 Away: 999');
+    });
+  });
 });
