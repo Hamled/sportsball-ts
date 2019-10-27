f77f24139d683c8ef150eeede3657446761f311a
Charles Ellis <hamled@hamled.name>
2019-10-26 21:21:28 -0700

    getScore - score is 1 after one homerun

--- test/index.test.ts
+++ test/index.test.ts
@@ -9,5 +9,10 @@ describe('Sportsball', () => {
     it('scores a tie game with no points', () => {
       expect((new Sportsball).getScore()).toEqual('Home: 0 Away: 0')
     })
+    it('score is 1 after one homerun', () => {
+      const sb = new Sportsball()
+      sb.addEntry(4)
+      expect(sb.getScore()).toEqual('Home: 0 Away: 1')
+    })
   })
 })
