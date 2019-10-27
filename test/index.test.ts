ff0986ec37e43c7ae8d1ca0de54337cdf553816a
Charles Ellis <hamled@hamled.name>
2019-10-27 15:48:45 -0700

    getScore method scores all frames with 1-5 points

--- test/index.test.ts
+++ test/index.test.ts
@@ -11,22 +11,12 @@ describe('sportsball', () => {
     it('returns getScore method', () => expect(sportsball().getScore).toBeDefined())
     describe('getScore method', () => {
       const zeroPointFrames = [
-        [],
-        [3],
-        [3,2],
-        [3,2,1],
-        [3,1],
-        [3,1,1],
-        [2],
-        [2,2],
-        [2,2,1],
-        [2,1],
-        [2,1,1],
-        [1],
-        [1,2],
-        [1,2,1],
-        [1,1],
-        [1,1,1],
+        // Non-point suffixes
+        [],[1],[2],[1,1],[2,1],
+        // Point suffixes (at index 5)
+                  [1,2],[1,1,1],[1,2,1],
+                  [2,2],[2,1,1],[2,2,1],
+        [3],[3,1],[3,2],[3,1,1],[3,2,1],
       ]
       it('scores all frames with 0 points', () => {
         zeroPointFrames.forEach(frame => {
@@ -35,6 +25,35 @@ describe('sportsball', () => {
           expect(sb.getScore()).toEqual('Home: 0 Away: 0')
         })
       })
+      const permutations = (n, f) => {
+        for(let p = 0; p < Math.pow(4, n); p++) {
+          let perm = []
+          for(let i = 0; i < n; i++) {
+            perm.unshift(Math.floor(p / Math.pow(4, i)) % 4 + 1)
+          }
+          f(perm, n)
+        }
+      }
+      const npSuffixes = zeroPointFrames.slice(0, 5)
+      const  pSuffixes = zeroPointFrames.slice(5)
+      const  maxPoints = 5
+      for(let i = 1; i <= maxPoints; i++) {
+        it(`scores all frames with ${i} point(s)`, () => {
+          permutations(i, frame => {
+            npSuffixes.forEach(suffix => {
+              const sb = sportsball()
+              const prefix = frame.slice(0, i-1);
+              [...prefix, 4, ...suffix].forEach(e => sb.addEntry(e))
+              expect(sb.getScore()).toEqual(`Home: Away: ${i}`)
+            })
+            pSuffixes.forEach(suffix => {
+              const sb = sportsball()
+              frame.concat(suffix).forEach(e => sb.addEntry(e))
+              expect(sb.getScore()).toEqual(`Home: Away: ${i}`)
+            })
+          })
+        })
+      }
     })
     it('returns addEntry method', () => expect(sportsball().addEntry).toBeDefined())
   })
