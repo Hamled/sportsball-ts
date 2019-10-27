8fc70a16dcd79225b6fd9cefcf24d8d6f1e8c1e9
Charles Ellis <hamled@hamled.name>
2019-10-27 14:20:52 -0700

    getScore method scores frames with 0 points

--- test/index.test.ts
+++ test/index.test.ts
@@ -7,6 +7,33 @@ describe('sportsball', () => {
   it('returns something', () => expect(sportsball()).toBeDefined())
   describe('called', () => {
     it('returns getScore method', () => expect(sportsball().getScore).toBeDefined())
+    describe('getScore method', () => {
+      const zeroPointFrames = [
+        [],
+        [3],
+        [3,2],
+        [3,2,1],
+        [3,1],
+        [3,1,1],
+        [2],
+        [2,2],
+        [2,2,1],
+        [2,1],
+        [2,1,1],
+        [1],
+        [1,2],
+        [1,2,1],
+        [1,1],
+        [1,1,1],
+      ]
+      it('scores all frames with 0 points', () => {
+        zeroPointFrames.forEach(frame => {
+          const sb = sportsball()
+          frame.forEach(e => sb.addEntry(e))
+          expect(sb.getScore()).toEqual('Home: 0 Away: 0')
+        })
+      })
+    })
     it('returns addEntry method', () => expect(sportsball().addEntry).toBeDefined())
   })
 })
