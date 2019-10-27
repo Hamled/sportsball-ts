90b65ad0f098b8471522e10f120a608b7a695d38
Charles Ellis <hamled@hamled.name>
2019-10-26 19:22:36 -0700

    Tests pass

--- src/sportsball.ts
+++ src/sportsball.ts
@@ -1,5 +1,8 @@
 export enum TurnResult {
   OUT,
+  SINGLE = 1,
+  DOUBLE = 2,
+  TRIPLE = 3,
   HOME_RUN
 }
 
@@ -11,25 +14,42 @@ export class Sportsball {
 
   private awayTurn: boolean = true;
 
+  private onBase: number = 0;
+
   getScore(): string {
     return `Home: ${this.home.points} Away: ${this.away.points}`;
   }
 
   addEntry(result: TurnResult) {
     const team = this.playingTeam();
-    if(result == TurnResult.HOME_RUN) {
-      team.points += 1;
-    } else {
+    if(result == TurnResult.OUT) {
       team.outs += 1;
+    } else {
+      this.scoreResult(result, team);
     }
 
     if(team.outs == 3) {
       this.awayTurn = !this.awayTurn;
+      this.onBase = 0;
       team.outs = 0;
     }
   }
 
   private playingTeam(): Team {
     return this.awayTurn ? this.away : this.home;
+  }
+
+  private scoreResult(result: TurnResult, team: Team) {
+    if(result == TurnResult.HOME_RUN) {
+      team.points += 1;
+    } else if(this.onBase == 3 && result >= TurnResult.SINGLE) {
+      team.points += 1;
+    } else if(this.onBase == 2 && result >= TurnResult.DOUBLE) {
+      team.points += 1;
+    } else if(this.onBase == 1 && result >= TurnResult.TRIPLE) {
+      team.points += 1;
+    } else {
+      this.onBase += 1;
+    }
   }
 }
