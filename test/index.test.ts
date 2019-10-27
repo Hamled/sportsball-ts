5b37aed7551b0fcb2c24d2c354ef328249399d7f
Charles Ellis <hamled@hamled.name>
2019-10-26 21:17:26 -0700

    Sportsball has addEntry method

--- test/index.test.ts
+++ test/index.test.ts
@@ -4,6 +4,7 @@ describe('Sportsball', () => {
   it('exists', () => expect(Sportsball).toBeDefined())
   it('can be constructed', () => expect(new Sportsball()).toBeDefined())
   it('has getScore method', () => expect(new Sportsball().getScore).toBeDefined())
+  it('has addEntry method', () => expect(new Sportsball().addEntry).toBeDefined())
   describe('#getScore', () => {
     it('scores a tie game with no points', () => {
       expect((new Sportsball).getScore()).toEqual('Home: 0 Away: 0')
