a0e1f35b71a8b7f32c15a94128c23645954186ac
Charles Ellis <hamled@hamled.name>
2019-10-26 21:30:50 -0700

    Tests pass

--- src/sportsball.ts
+++ src/sportsball.ts
@@ -2,7 +2,7 @@ export class Sportsball {
   private score = 0;
 
   getScore() {
-    return `Home: 0 Away: ${this.score / 4}`
+    return `Home: 0 Away: ${Math.floor(this.score / 4)}`
   }
   addEntry(score: number) {
     this.score += score
