36c0794635794dbb9fdfb3686738884dbb0be73c
Charles Ellis <hamled@hamled.name>
2019-10-27 09:55:02 -0700

    sportsball called returns addEntry method

--- test/index.test.ts
+++ test/index.test.ts
@@ -6,5 +6,6 @@ describe('sportsball', () => {
   it('returns something', () => expect(sportsball()).toBeDefined())
   describe('called', () => {
     it('returns getScore method', () => expect(sportsball().getScore).toBeDefined())
+    it('returns addEntry method', () => expect(sportsball().addEntry).toBeDefined())
   })
 })
