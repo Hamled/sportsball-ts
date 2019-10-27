7dccd75a97f6e86a0a0b4549e90d371065bb881d
Charles Ellis <hamled@hamled.name>
2019-10-26 22:19:37 -0700

    #getScore scores frames with no points

--- test/index.test.ts
+++ test/index.test.ts
@@ -7,6 +7,33 @@ describe('Sportsball', () => {
   it('can be constructed', () => expect(new Sportsball()).toBeDefined())
   describe('constructed', () => {
     it('has #getScore method', () => expect(new Sportsball().getScore).toBeDefined())
+    describe('#getScore', () => {
+      const expectScore = (score: number, entries: number[]) => {
+        const sb = new Sportsball()
+        entries.map(e => sb.addEntry(e))
+        expect(sb.getScore()).toEqual(`Home: 0 Away: ${score}`)
+      }
+      it('scores frames with no points', () => {
+        [
+          [],
+          [3],
+          [2],
+          [1],
+          [3,2],
+          [3,1],
+          [2,2],
+          [2,1],
+          [1,2],
+          [1,1],
+          [3,2,1],
+          [3,1,1],
+          [2,2,1],
+          [2,1,1],
+          [1,2,1],
+          [1,1,1]
+        ].forEach(entries => expectScore(0, entries))
+      })
+    })
     it('has #addEntry method', () => expect(new Sportsball().addEntry).toBeDefined())
   })
 })
