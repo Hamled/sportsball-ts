b883405ea5e6ee7e7005230502cb74feba3fa731
Charles Ellis <hamled@hamled.name>
2019-10-26 17:33:28 -0700

    Test for no turns at bat

--- test/sportsball.test.ts
+++ test/sportsball.test.ts
@@ -9,5 +9,9 @@ describe('Sportsball', () => {
     it('is defined', () => {
       expect(new Sportsball().getScore).toBeDefined();
     });
+
+    it('with no turns it returns score of 0-0', () => {
+      expect(new Sportsball().getScore()).toEqual('Home: 0 Away: 0');
+    });
   });
 });
