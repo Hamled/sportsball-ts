c428a61ddf9783e534d5043dc65a6fa0a2892b80
Charles Ellis <hamled@hamled.name>
2019-10-27 09:53:05 -0700

    sportsball returns something

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,4 +1,5 @@
 const sportsball = () => {}
 describe('sportsball', () => {
   it('exists', () => expect(sportsball).toBeDefined())
+  it('returns something', () => expect(sportsball()).toBeDefined())
 })
