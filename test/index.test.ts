5959029bcb58e167a11b202f6db9f924a4a6861e
Charles Ellis <hamled@hamled.name>
2019-10-27 09:53:13 -0700

    Tests pass

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,4 +1,4 @@
-const sportsball = () => {}
+const sportsball = () => ({})
 describe('sportsball', () => {
   it('exists', () => expect(sportsball).toBeDefined())
   it('returns something', () => expect(sportsball()).toBeDefined())
