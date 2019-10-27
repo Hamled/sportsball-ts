e6f127c23621f0e44017d62a4f7d620a57a28af1
Charles Ellis <hamled@hamled.name>
2019-10-26 21:09:42 -0700

    getScore scores a tie game with no points

--- test/index.test.ts
+++ test/index.test.ts
@@ -4,4 +4,9 @@ describe('Sportsball', () => {
   it('exists', () => expect(Sportsball).toBeDefined())
   it('can be constructed', () => expect(new Sportsball()).toBeDefined())
   it('has getScore method', () => expect(new Sportsball().getScore).toBeDefined())
+  describe('#getScore', () => {
+    it('scores a tie game with no points', () => {
+      expect((new Sportsball).getScore()).toEqual('Home: 0 Away: 0')
+    })
+  })
 })
