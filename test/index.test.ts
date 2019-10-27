7258b80a0286223956cf7a9485c93eaaff41ef09
Charles Ellis <hamled@hamled.name>
2019-10-26 21:06:13 -0700

    Sportsball has getScore method

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,4 +3,5 @@ import {Sportsball} from '../src/sportsball';
 describe('Sportsball', () => {
   it('exists', () => expect(Sportsball).toBeDefined())
   it('can be constructed', () => expect(new Sportsball()).toBeDefined())
+  it('has getScore method', () => expect(new Sportsball().getScore).toBeDefined())
 })
