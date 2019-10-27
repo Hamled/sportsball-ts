f4a7dc715fa882efbb9bbf3142c67b2de48ccd48
Charles Ellis <hamled@hamled.name>
2019-10-26 22:10:41 -0700

    Sportsball constructed has #getScore method

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,4 +2,7 @@ class Sportsball {}
 describe('Sportsball', () => {
   it('exists', () => expect(Sportsball).toBeDefined())
   it('can be constructed', () => expect(new Sportsball()).toBeDefined())
+  describe('constructed', () => {
+    it('has #getScore method', () => expect(new Sportsball().getScore).toBeDefined())
+  })
 })
