4f710992e8fb6afdff8d8aa59f11b26885d0bc2c
Charles Ellis <hamled@hamled.name>
2019-10-27 09:54:18 -0700

    sportsball called returns getScore method

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,4 +2,7 @@ const sportsball = () => ({})
 describe('sportsball', () => {
   it('exists', () => expect(sportsball).toBeDefined())
   it('returns something', () => expect(sportsball()).toBeDefined())
+  describe('called', () => {
+    it('returns getScore method', () => expect(sportsball().getScore).toBeDefined())
+  })
 })
