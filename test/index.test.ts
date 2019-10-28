64e96ed84321463cb7799c7bc526aaefa0113fd4
Charles Ellis <hamled@hamled.name>
2019-10-28 14:32:51 -0700

    getScore scores frames with 1-5 points

--- test/index.test.ts
+++ test/index.test.ts
@@ -24,6 +24,36 @@ describe('sportsball', () => {
           expect(sb.getScore()).toEqual('Home: 0 Away: 0')
         })
       })
+      const eachPermutation = (n, f) => {
+        for(let i = 0; i < Math.pow(4, n); i++) {
+          const perm = []
+          for(let d = 0; d < n; d++) {
+            perm.unshift(Math.floor(i / Math.pow(4, d)) % 4 + 1)
+          }
+          f(perm)
+        }
+      }
+      const maxPoints = 5
+      for(let points = 1; points <= maxPoints; points++) {
+        it(`scores frames with ${points} points`, () => {
+          const npSuffixes = zeroPointFrames.slice(0, 5)
+          const  pSuffixes = zeroPointFrames.slice(5)
+          const checkFrame = frame => {
+            const sb = sportsball()
+            frame.forEach(e => sb.addEntry(e))
+
+            expect(sb.getScore()).toEqual(`Home: 0 Away: ${points}`)
+          }
+
+          eachPermutation(points, prefix => {
+            npSuffixes.map(s => [4, ...s]).forEach(suffix => {
+              checkFrame(prefix.slice(0, points - 1).concat(suffix))
+            })
+
+            pSuffixes.forEach(suffix => checkFrame(prefix.concat(suffix)))
+          })
+        })
+      }
     })
   })
 })
