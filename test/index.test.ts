8d29adea80efcd05e2041d5c056d4e18a9f1d0cb
Charles Ellis <hamled@hamled.name>
2019-10-28 14:55:20 -0700

    getScore scores even frames for home

--- test/index.test.ts
+++ test/index.test.ts
@@ -60,6 +60,15 @@ describe('sportsball', () => {
 
         expect(sb.getScore()).toEqual('Home: 0 Away: 1')
       })
+      it('scores even frames for home', () => {
+        const sb = sportsball()
+        sb.addEntry(0)
+        sb.addEntry(0)
+        sb.addEntry(0)
+        sb.addEntry(4)
+
+        expect(sb.getScore()).toEqual('Home: 1 Away: 0')
+      })
     })
   })
 })
