c0c6747ff8378aedec50cadc169257a231c64ac1
Charles Ellis <hamled@hamled.name>
2019-10-29 15:58:23 -0700

    tests pass

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
+const BASES = 3
 export const sportsball = () => {
   let runners = 0
-  let points = 0
+  let unscoredRunners = 0
+  const score = () => runners - unscoredRunners
+  const runnersNeeded = (entry) => BASES + 1 - entry
+
   return {
     addEntry(entry) {
       runners++
-      if(entry === 4) points += runners
+      if(runnersNeeded(entry) - unscoredRunners < 1) {
+        unscoredRunners = runnersNeeded(entry)
+      } else {
+        unscoredRunners++
+      }
     },
     getScore() {
-      return `Home: 0 Away: ${points}`
+      return `Home: 0 Away: ${score()}`
     },
   }
 }
