3c37f48d38e336b1ad559cfe3bb00c4e8b6a7ecd
Charles Ellis <hamled@hamled.name>
2019-10-28 19:21:52 -0700

    Tests pass

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,11 @@ export const sportsball = () => {
   let frame = Array(FRAME_LEN)
   let cursor = FRAME_LEN
 
+  let away = 0
+  let home = 0
+  let isAway = true
+  let outs = 0
+
   const scoreFrame = () => {
     let need = 3
     for(let i = cursor; i < FRAME_LEN; i++) {
@@ -18,11 +23,26 @@ export const sportsball = () => {
 
   return {
     addEntry(entry) {
-      cursor--
-      frame[cursor] = entry
+      if(entry !== 0) {
+        cursor--
+        frame[cursor] = entry
+        return
+      }
+
+      outs += 1
+      if(outs > 2) {
+        const score = scoreFrame()
+        isAway ? away += score : home += score
+        isAway = !isAway
+        outs = 0
+        cursor = FRAME_LEN
+      }
     },
     getScore() {
-      return `Home: 0 Away: ${scoreFrame()}`
+      const score = scoreFrame()
+      const curAway = isAway ? away + score : away
+      const curHome = isAway ? home : home + score
+      return `Home: ${curHome} Away: ${curAway}`
     }
   }
 }
