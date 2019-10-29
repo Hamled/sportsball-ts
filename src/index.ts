00f93de2c639680681174c363bca8acdbbdd0dd5
Charles Ellis <hamled@hamled.name>
2019-10-28 19:16:51 -0700

    Tests pass

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,12 @@ export const sportsball = () => {
   let cursor = FRAME_LEN
 
   const scoreFrame = () => {
+    let need = 3
     for(let i = cursor; i < FRAME_LEN; i++) {
       const e = frame[i]
       if(e === 4) return FRAME_LEN - i
-      if(e === 3) return FRAME_LEN - i - 1
+      if(e === 3 || need < 2) return FRAME_LEN - i - 1
+      if(e === 2) need -= e
     }
 
     return 0
