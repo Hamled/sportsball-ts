d35eef2d964d68dfafe0220d233530078f774814
Charles Ellis <hamled@hamled.name>
2019-10-28 20:59:35 -0700

    getScore scores a frames with no entries

--- test/index.test.ts
+++ test/index.test.ts
@@ -7,6 +7,13 @@ describe('sportsball', () => {
     it('returns an object', () => expect(sportsball(0)).toBeInstanceOf(Object))
     it('has an addEntry method', () => expect(sportsball(0).addEntry).toBeInstanceOf(Function))
     it('has a getScore method', () => expect(sportsball(0).getScore).toBeInstanceOf(Function))
+    describe('getScore', () => {
+      it('scores frames with no entries', () => {
+        const sb = sportsball(0);
+
+        expect(sb.getScore()).toEqual('Home: 0 Away: 0')
+      })
+    })
   })
   it('can be called with number of bases', () => expect(() => sportsball(3)).not.toThrowError())
 })
