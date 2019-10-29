31f17387d66d09159354cd845312c39ce0b14c6a
Charles Ellis <hamled@hamled.name>
2019-10-29 15:29:59 -0700

    sportsball retursn an object

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,4 +3,5 @@ import {sportsball} from '../src'
 describe('sportsball', () => {
   it('exists', () => expect(sportsball).toBeDefined())
   it('can be called', () => expect(sportsball).toBeInstanceOf(Function))
+  it('returns an object', () => expect(sportsball()).toBeInstanceOf(Object))
 })
