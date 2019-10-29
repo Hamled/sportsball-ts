6a67a3807ad7380b4c607775c8b0bb628a70c5ea
Charles Ellis <hamled@hamled.name>
2019-10-28 19:15:11 -0700

    Tests pass

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,25 @@
-export const sportsball = () => ({
-  addEntry() {},
-  getScore() {
-    return 'Home: 0 Away: 0'
+const FRAME_LEN = 4
+
+export const sportsball = () => {
+  let frame = Array(FRAME_LEN)
+  let cursor = FRAME_LEN
+
+  const scoreFrame = () => {
+    for(let i = cursor; i < FRAME_LEN; i++) {
+      const e = frame[i]
+      if(e === 4) return FRAME_LEN - i
+    }
+
+    return 0
   }
-})
+
+  return {
+    addEntry(entry) {
+      cursor--
+      frame[cursor] = entry
+    },
+    getScore() {
+      return `Home: 0 Away: ${scoreFrame()}`
+    }
+  }
+}
