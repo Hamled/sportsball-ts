87c7295bf00e40466331d1bc85f6b274bbef7ebc
Charles Ellis <hamled@hamled.name>
2019-10-29 15:38:09 -0700

    tests pass

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
-export const sportsball = () => ({
-  addEntry() {},
-  getScore() {
-    return 'Home: 0 Away: 0'
-  },
-})
+export const sportsball = () => {
+  let runners = 0
+  let points = 0
+  return {
+    addEntry(entry) {
+      runners++
+      if(entry === 4) points += runners
+    },
+    getScore() {
+      return `Home: 0 Away: ${points}`
+    },
+  }
+}
