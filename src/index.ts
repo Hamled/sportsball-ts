abc6863f73bae27fab65448cf74186f04e895212
Charles Ellis <hamled@hamled.name>
2019-10-29 12:44:59 -0700

    tests pass

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,15 @@ const BASES = 3
 export const sportsball = () => {
   let entries = 0
   let last = BASES + 1
+  let load = 0
 
-  const score = () => entries - (BASES + 1 - last)
+  const score = () => (entries - load) + Math.max(0, load - (BASES + 1 - last))
 
   return {
     addEntry(entry) {
       entries++
       last = entry
+      load = (entry === 4) ? 0 : load + 1
     },
     getScore() {
       return `Home: 0 Away: ${score()}`
