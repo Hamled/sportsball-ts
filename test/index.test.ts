6a2b1d768cecdd18b35bb36a1077f63ea41a4ee5
Charles Ellis <hamled@hamled.name>
2019-10-29 12:36:55 -0700

    getScore scores 1 point for a home run

--- test/index.test.ts
+++ test/index.test.ts
@@ -13,6 +13,12 @@ describe('sportsball', () => {
 
         expect(sb.getScore()).toEqual('Home: 0 Away: 0')
       })
+      it('scores 1 point for a home run', () => {
+        const sb = sportsball()
+        sb.addEntry(4)
+
+        expect(sb.getScore()).toEqual('Home: 0 Away: 1')
+      })
     })
   })
 })
