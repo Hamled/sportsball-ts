200a0af67e0f15c3afaedf674e09e348da554df2
Charles Ellis <hamled@hamled.name>
2019-10-28 20:54:12 -0700

    sportsball can be called with number of bases

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,4 +3,5 @@ import {sportsball} from '../src'
 describe('sportsball', () => {
   it('exists', () => expect(sportsball).toBeDefined())
   it('can be called', () => expect(sportsball).toBeInstanceOf(Function))
+  it('can be called with number of bases', () => expect(() => sportsball(3)).not.toThrowError())
 })
