7a7efb7a3629b0b6917a261aec37b2f6bdcf13e3
Charles Ellis <hamled@hamled.name>
2019-10-30 10:34:21 -0700

    tests pass

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,36 @@
 const BASES = 3
 export const sportsball = () => {
-  let runners = 0
-  let unscored = 0
-  let last = 0
+  let runners, unscored, last, outs
+  let away = 0
+  let home = 0
+  let isAway = false
+
   const score = () => runners - unscored
   const runnersToNextPoint = () => BASES + 1 - last
+  const resetFrame = () => {
+    runners = unscored = last = outs = 0
+    isAway = !isAway
+  }
 
+  resetFrame()
   return {
     addEntry(entry) {
+      if(entry === 0) {
+        if(++outs > 2) {
+          isAway ? away += score() : home += score()
+          resetFrame()
+        }
+        return
+      }
+
       runners++
       last = entry
       unscored = Math.min(runnersToNextPoint(), unscored + 1)
     },
     getScore() {
-      return `Home: 0 Away: ${score()}`
+      const curAway = isAway ? away + score() : away
+      const curHome = isAway ? home : home + score()
+      return `Home: ${curHome} Away: ${curAway}`
     },
   }
 }
