9be8b92f924c5e4b6e1c9c3c4916bf819a40ba87
Charles Ellis <hamled@hamled.name>
2019-10-30 10:30:36 -0700

    tests pass

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,15 @@ const BASES = 3
 export const sportsball = () => {
   let runners = 0
   let unscored = 0
+  let last = 0
   const score = () => runners - unscored
+  const runnersToNextPoint = () => BASES + 1 - last
 
   return {
     addEntry(entry) {
       runners++
-      unscored++
-      if(entry >= BASES) unscored = (BASES + 1 - entry)
+      last = entry
+      unscored = Math.min(runnersToNextPoint(), unscored + 1)
     },
     getScore() {
       return `Home: 0 Away: ${score()}`
