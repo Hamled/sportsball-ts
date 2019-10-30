3fed1b1ed5c3b5c8cc898be506875717b119656a
Charles Ellis <hamled@hamled.name>
2019-10-30 10:20:36 -0700

    getScore scores points for 0+ runners before a home run

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,12 +1,17 @@
 import {sportsball} from '../src'
 
 describe('sportsball', () => {
-  it('has an addEntry method', () => expect(sportsball().addEntry).toBeInstanceOf(Function))
   describe('getScore', () => {
     it('scores 0 points for no runners', () => {
       const sb = sportsball()
 
       expect(sb.getScore()).toEqual('Home: 0 Away: 0')
+    })
+    it('scores points for 0+ runners before a home run', () => {
+      const sb = sportsball();
+      [3, 2, 1, 4, 3, 2, 1].forEach(e => sb.addEntry(e))
+
+      expect(sb.getScore()).toEqual('Home: 0 Away: 4')
     })
   })
 })
