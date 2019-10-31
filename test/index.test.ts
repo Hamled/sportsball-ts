8937fb74042031c4b663fba24059ab5233195c60
Charles Ellis <hamled@hamled.name>
2019-10-31 11:24:56 -0700

    sportsball has a getScore method

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,5 @@
 import {sportsball} from '../src'
 
 describe('sportsball', () => {
-  it('returns an object', () => expect(sportsball()).toBeInstanceOf(Object))
+  it('has a getScore method', () => expect(sportsball().getScore).toBeInstanceOf(Function))
 })
