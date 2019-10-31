c66e7ddffb14b468450d018edeaddc49721db4f1
Charles Ellis <hamled@hamled.name>
2019-10-31 11:27:22 -0700

    sportsball has an addEntry method

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,7 @@
 import {sportsball} from '../src'
 
 describe('sportsball', () => {
+  it('has an addEntry method', () => expect(sportsball().addEntry).toBeInstanceOf(Function))
   describe('getScore', () => {
     it('scores 0 points for no runners', () => {
       const sb = sportsball()
