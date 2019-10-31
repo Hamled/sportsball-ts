62d8dabb68095e7cb5a4b2a8064ea26f27b99812
Charles Ellis <hamled@hamled.name>
2019-10-31 11:23:43 -0700

    sportsball can be called

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,5 @@
 import {sportsball} from '../src'
 
 describe('sportsball', () => {
-  it('exists', () => expect(sportsball).toBeDefined())
+  it('can be called', () => expect(sportsball).toBeInstanceOf(Function))
 })
